9c3f851ffb6871610b84dfa95c677b5215287a12
Rian Carter <R.Carter86@hotmail.com>
2022-01-25 13:27:35 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,34 @@
-function divide(number1) {
+function tsptbsp(number1) {
   return number1 / 3;
 }
 
-function divide(number1) {
-  return number1 / 16;
-}
+// function multiply(number1) {
+//   return number1 * 3;
+// }
+
+// function divide(number1) {
+//   return number1 / 16;
+// }
 
 $(document).ready(function() {
-  $("form#divide").submit(function(event) {
+  $("form#tsptbsp").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#div").val());
-    const result = divide(number1, 3);
-    $("#output-div").text(result);
+    const number1 = parseInt($("#tsptbsp").val());
+    const result = tsptbsp(number1, 3);
+    $("#output-tsptbsp").text(result);
   });
 
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#div").val());
-    const result = divide(number1, 16);
-    $("#output-div").text(result);
-  });
+  // $("form#multiply").submit(function(event) {
+  //   event.preventDefault();
+  //   const number1 = parseInt($("#mul").val());
+  //   const result = multiply(number1, 16);
+  //   $("#output-mul").text(result);
+  // });
+
+  // $("form#divide").submit(function(event) {
+  //   event.preventDefault();
+  //   const number1 = parseInt($("#div").val());
+  //   const result = divide(number1, 16);
+  //   $("#output-div").text(result);
+  // });
 });
